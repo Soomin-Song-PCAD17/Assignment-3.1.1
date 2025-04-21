891c968983d88181726508af036ccc59c211e258
Soomin Song <song.soomin.418@outlook.com>
2025-04-21 08:27:11 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Text;
+
+Console.WriteLine(ReturnEvenNumbers(0, 100));
+
+string ReturnEvenNumbers(int number1, int number2)
+{
+    StringBuilder sb = new StringBuilder();
+    int pointer = number1;
+    if (number1 % 2 == 1) { pointer++; }
+    while (pointer < number2)
+    {
+        sb.Append($"{pointer} ");
+        pointer = pointer + 2;
+    }
+
+    return sb.ToString();
+}
